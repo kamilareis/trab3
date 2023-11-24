255f2df18d15be7e5a9318563deae9d2dbf3e746
Kamilareis <kamilareis2@gmail.com>
2023-11-24 00:03:07 -0300

    Incluso o bootstrap

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+pip install..
+flask
+flask_login
+flask_wtf
+email_validator
+flask_bcrypt
+flask_sqlalchemy
+
+"""
+from instagram import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
